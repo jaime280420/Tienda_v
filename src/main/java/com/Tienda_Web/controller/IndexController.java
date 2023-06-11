7f4bf20b66a7219280ccea305ed0cde324c1b26f
Usuario <Usuario@DESKTOP-MQS24AD>
2023-06-11 00:02:11 -0600

    Finalizacion Semana 4

---
+++ src/main/java/com/Tienda_Web/controller/IndexController.java
@@ -0,0 +1,18 @@
+
+package com.Tienda_Web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/")
+    public String page(Model model) {
+        model.addAttribute("mensaje", "Hola desde el controlador ");
+        return "index";
+    }
+    
+}
